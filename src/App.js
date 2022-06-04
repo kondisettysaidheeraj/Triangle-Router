e5c05b87254fd6480c395e4c3419d4b87d9534ad
Kondisetty Sai Dheeraj <105906908+kondisettysaidheeraj@users.noreply.github.com>
2022-06-04 20:48:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Boot from './components/Boot'
-import Home from './components/Home'
+
 import { Area } from "./Area";
 import { Triangle } from "./Triangle";
 import { Hypotense } from "./Hypotense";
 import Quiz from "./Quiz";
 import Navbar from "./Navbar";
 
-import Inputs from './components/Inputs'
+
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 
 function App() {
@@ -19,19 +18,10 @@ function App() {
   <div className="App">
   
     <Switch>
-      
-      {/* <Route path="/" exact={true} component={Home}/>
-      <Route path="/Details" exact={true} component={Inputs}/>
-      <Route path="/Standards" exact={true} component={Boot}/> */}
-
-      
       <Route path="/Area" exact={true} component={Area}/>
       <Route path="/Hypotense" exact={true} component={Hypotense}/>
       <Route path="/Triangle" exact={true} component={Triangle}/>
       <Route path="/" exact={true} component={Quiz}/>
-      
-      
-      {/* <Boot/> */}
       </Switch>
        
       </div>
